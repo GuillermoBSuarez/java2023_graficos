cbe87edba5af4e96b56e6dcbd545afcb863a50ae
tarde <tarde@A13E31>
2023-10-27 20:52:23 +0200

    27/10/2023

--- 10_seleccion_empleados/src/view/JEmpleados.java
+++ 10_seleccion_empleados/src/view/JEmpleados.java
@@ -44,6 +44,7 @@ public class JEmpleados extends JFrame {
 	 * Create the frame.
 	 */
 	public JEmpleados() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
